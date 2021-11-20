6be8679e1b392e677865df365a2380ac55d8a935
Rubin James <rubin.james@smu.ca>
2021-11-20 13:03:49 -0400

    Initial commit

---
+++ mcda-tut-rubin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mcda_tut_rubin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
